b7d18e97d80a122e0af0838513604bbdd578a96a
penguin <penguin@penguinoMacBook.elecom>
2021-02-22 02:42:27 +0900

    add

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+
+  has_many :posts, dependent: :destroy
+  has_many :likes
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, length: { maximum: 50 }
+
+  # ==========ここから追加する==========
+  def update_without_current_password(params, *options)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+  # ==========ここまで追加する==========
+
+end
